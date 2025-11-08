df175482e5aaaecd34e1aebc2259c58b37cbbd75
Aaraan Mahmood <151668629+aairhann17@users.noreply.github.com>
2025-11-08 12:49:44 -0500

    Add Product model and update string representation

--- db/models.py
+++ db/models.py
@@ -7,10 +7,11 @@ except Exception:
     sys.exit()
 
 
-# Sample User model
-class User(models.Model):
-    name = models.CharField(max_length=50, default="Dan")
+# Product model
+class Product(models.Model):
+    upc = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.name
-
+        return f"{self.name} (${self.price})"
