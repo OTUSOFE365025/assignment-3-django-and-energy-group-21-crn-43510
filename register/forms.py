7abb4a11e4a488e2ee2e6f78b5469c0de039c3df
Aaraan Mahmood <151668629+aairhann17@users.noreply.github.com>
2025-11-10 21:18:52 -0500

    Delete register directory

--- register/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms # Import the forms library from Django
-
-# Scans the form
-class ScanForm(forms.Form):
-    upc = forms.CharField(label='UPC', max_length=50)
--- register/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.scan, name='scan'),
-    path('load/', views.load_products, name='load_products'),
-]
